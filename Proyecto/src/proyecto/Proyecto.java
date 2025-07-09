5f7418967df29fafdd24be6dfb5e9a51cd4a8df7
emmacv06 <emmavillaltac@gmail.com>
2025-07-09 01:57:57 -0600

    Update Proyecto.java

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
 package proyecto;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,12 +16,18 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        String continuar = "1";        
         Cabina c1 = new Cabina(1);
-        c1.reservarHora(10, "ID001");
-        c1.reservarHora(14, "ID002");
+
+        while (continuar.equals("1")) {
+        
+        String horaTexto = JOptionPane.showInputDialog("que hora reservas?");
+        int hora = Integer.parseInt(horaTexto);
+        //Cabina c1 = new Cabina(1);
+        c1.reservarHora(hora, "ID001");
         c1.mostrarHorarios();
-
+        
+        continuar = JOptionPane.showInputDialog("Si desea otra reserva digite 1");
+        }
     }
-
 }
